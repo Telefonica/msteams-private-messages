0999cd80dac569135ca40e79236658c5656212bc
Manu Artero Anguita <manutero.developer@gmail.com>
2021-02-01 15:57:42 +0100

    fix: connecting to remote BotFramework when !LOCAL
    bug: at createBotAdapter() we were considering 'false' as truthy
    from: process.env.LOCAL || false
    to: process.env.LOCAL === 'true' || false
    
    extra: renamed env vars (at .env.template)

--- src/bot-adapter.js
+++ src/bot-adapter.js
@@ -2,23 +2,21 @@ const { BotFrameworkAdapter } = require('botbuilder')
 const { log } = require('./log')
 
 const createBotAdapter = () => {
-  const runningLocally = process.env.LOCAL || false
+  const runningLocally = process.env.LOCAL === 'true' || false
 
   const adapter = new BotFrameworkAdapter({
-    appId: runningLocally ? null : process.env.MicrosoftAppId,
-    appPassword: runningLocally ? null : process.env.MicrosoftAppPassword
+    appId: runningLocally ? null : process.env.MICROSOFT_APP_ID,
+    appPassword: runningLocally ? null : process.env.MICROSOFT_APP_PASSWORD
   })
 
   adapter.onTurnError = async (context, error) => {
     log.error(error, '[onTurnError] unhandled erro')
-
     await context.sendTraceActivity(
       'OnTurnError Trace',
       `${error}`,
       'https://www.botframework.com/schemas/error',
       'TurnError'
     )
-
     await context.sendActivity('The bot encountered an error or bug.')
   }
 
