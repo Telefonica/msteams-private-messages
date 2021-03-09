61efbdd891b9c5bc5a4e3a2e2b8cfb2813904b1b
Manu Artero Anguita <manutero.developer@gmail.com>
2021-03-09 09:49:25 +0100

    refactor: simplify code style at handlers.js

--- src/server/handlers.js
+++ src/server/handlers.js
@@ -12,52 +12,6 @@ const { createConversationHelper } = require('./conversation-helper')
 const createHandlers = (adapter, storage, bot) => {
   const conversationHelper = createConversationHelper(adapter)
 
-  /**
-   * @param {import("restify").Request} req,
-   * @param {import("restify").Response} res
-   */
-  const processMessage = (req, res) =>
-    adapter.processActivity(req, res, async turnContext => {
-      /* route to main dialog */
-      await bot.run(turnContext)
-    })
-
-  /**
-   * @param {string} user
-   * @param {string} message
-   * @param {boolean} mention
-   */
-  const notify = async (user, message, mention) => {
-    const conversationRef = await storage.getConversation(user)
-    if (!conversationRef) {
-      throw new NotFoundError(`user not found: '${user}'`)
-    }
-    await conversationHelper.sendMessage(conversationRef, message, mention)
-    return conversationRef.conversation.id
-  }
-
-  /**
-   * @param {string} topic
-   * @param {string} message
-   * @param {boolean} mention
-   */
-  const broadcast = async (topic, message, mention) => {
-    const subscribers = await storage.getSubscribers(topic)
-    const conversationKeys = []
-    for (const user of subscribers) {
-      const conversationRef = await storage.getConversation(user)
-      if (!conversationRef) {
-        log.warn(
-          `weird status: user "${user}" seems to be subscribed to "${topic}" but conversationRef not found. SKIPPING.`
-        )
-      } else {
-        conversationKeys.push(conversationRef.conversation.id)
-        conversationHelper.sendMessage(conversationRef, message, mention)
-      }
-    }
-    return conversationKeys
-  }
-
   const getTopics = async () => {
     const topicNames = await storage.listTopics()
     /** @type {{[name: string]: string[]}} */
@@ -73,37 +27,60 @@ const createHandlers = (adapter, storage, bot) => {
     return topics
   }
 
-  const getUsers = async () => {
-    const users = await storage.listUsers()
-    return users
-  }
-
-  /**
-   * @param {string} topic
-   */
-  const createTopic = async topic => {
-    await storage.registerTopic(topic) // may already exist
-    return getTopics()
-  }
-
-  /**
-   * @param {string} user
-   * @param {string} topic
-   */
-  const forceSubscription = async (user, topic) => {
-    await storage.subscribe(user, topic)
-    const subscribers = await storage.getSubscribers(topic)
-    return subscribers
-  }
-
   return {
-    processMessage,
-    notify,
-    broadcast,
+    processMessage: (req, res) =>
+      adapter.processActivity(req, res, async turnContext => {
+        /* route to main dialog */
+        await bot.run(turnContext)
+      }),
+
+    notify: async (user, message, mention) => {
+      const conversationRef = await storage.getConversation(user)
+      if (!conversationRef) {
+        throw new NotFoundError(`user not found: '${user}'`)
+      }
+      await conversationHelper.sendMessage(conversationRef, message, mention)
+      return conversationRef.conversation.id
+    },
+
+    broadcast: async (topic, message, includeMention) => {
+      const subscribers = await storage.getSubscribers(topic)
+      const conversationKeys = []
+      for (const user of subscribers) {
+        const conversationRef = await storage.getConversation(user)
+        if (!conversationRef) {
+          log.warn(
+            `weird status: user "${user}" seems to be subscribed to "${topic}" but conversationRef not found. SKIPPING.`
+          )
+        } else {
+          conversationKeys.push(conversationRef.conversation.id)
+          conversationHelper.sendMessage(
+            conversationRef,
+            message,
+            includeMention
+          )
+        }
+      }
+      return conversationKeys
+    },
+
     getTopics,
-    getUsers,
-    createTopic,
-    forceSubscription
+
+    getUsers: async () => {
+      const users = await storage.listUsers()
+      return users
+    },
+
+    createTopic: async topic => {
+      await storage.registerTopic(topic) // may already exist
+      return getTopics()
+    },
+
+    forceSubscription: async (user, topic) => {
+      await storage.subscribe(user, topic)
+      const subscribers = await storage.getSubscribers(topic)
+      return subscribers
+    }
   }
 }
 
